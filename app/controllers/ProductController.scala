dd208c2828302c69f32c6abe5c8357d98572b3c8
jordi <jordikiter@hotmail.es>
2018-12-24 11:54:00 +0000

    Add logger

--- app/controllers/ProductController.scala
+++ app/controllers/ProductController.scala
@@ -2,6 +2,7 @@ package controllers
 
 import javax.inject.{Inject, Singleton}
 import models.Product
+import play.api.Logger
 import play.api.libs.json._
 import play.api.mvc.{AbstractController, ControllerComponents}
 
@@ -10,12 +11,15 @@ class ProductController @Inject()(cc: ControllerComponents)(implicit assetsFinde
   extends AbstractController(cc) {
 
   val products = List(Product(1, "Prod1"), Product(2, "Prod2"))
+  val logger = Logger(getClass)
 
   def show = Action {
+    logger.debug("show - send products")
     Ok(views.html.product(products))
   }
 
   def showJson = Action { implicit request =>
+    logger.debug("showJson - parsing")
     Ok(Json.toJson(Product(1, "Prod")))
   }
 }
