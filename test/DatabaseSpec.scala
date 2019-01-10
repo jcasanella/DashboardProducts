6bac13ffacc240d94fc83d4b4f84b7e775c59c84
jordi <jordikiter@hotmail.es>
2019-01-10 22:41:28 +0000

    Populate list box from Postgres Unit test postgresql and h2

--- test/DatabaseSpec.scala
+++ test/DatabaseSpec.scala
@@ -1,19 +1,31 @@
 import dao.ProductDAO
 import models.Product
 import org.scalatest.{FlatSpec, Matchers}
+import org.scalatestplus.play.guice.GuiceOneAppPerSuite
 import play.api.Application
+import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.test.WithApplicationLoader
+
 import scala.concurrent.duration.DurationInt
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.{Await, Future}
 
-class DatabaseSpec extends FlatSpec with Matchers {
+class DatabaseSpec extends FlatSpec with Matchers with GuiceOneAppPerSuite {
 
-  "Content Table products" should  "be the same as testProducts" in new WithApplicationLoader() {
-    val app2dao = Application.instanceCache[ProductDAO]
-    val dao = app2dao(app)
+  implicit override lazy val app = new GuiceApplicationBuilder()
+    .configure(Map(
+      "slick.dbs.default.profile"->"slick.jdbc.H2Profile$",
+      "slick.dbs.default.db.driver"->"org.h2.Driver",
+      "slick.dbs.default.db.url"->"jdbc:h2:mem:play;MODE=MYSQL;DB_CLOSE_DELAY=-1"
+    )
+    )
+    .build()
 
-    dao.init()
+  def productDao(implicit app: Application) = Application.instanceCache[ProductDAO].apply(app)
+
+  "Content of products table" should  "be the same as testProducts set" in {
+
+    productDao.init()
 
     val testProducts = Set(
       Product(1, "product1"),
@@ -21,8 +33,8 @@ class DatabaseSpec extends FlatSpec with Matchers {
       Product(3, "product3")
     )
 
-    Await.result(Future.sequence(testProducts.map(dao.insert)), 1 seconds)
-    val stored = Await.result(dao.all(), 1 seconds)
+    Await.result(Future.sequence(testProducts.map(productDao.insert)), 1 seconds)
+    val stored = Await.result(productDao.all(), 1 seconds)
 
     stored.toSet == testProducts
   }
